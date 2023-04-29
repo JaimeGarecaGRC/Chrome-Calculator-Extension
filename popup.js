e5fe84fc1ba840e06efe9a5eefc7e85e205f4eb7
JaimeGarecaGRC <me@jaimegareca.com>
2023-04-29 02:04:33 -0400

    Numbers and arithmetic operators are being displayed and working correctly

--- popup.js
+++ popup.js
@@ -1,5 +1,6 @@
 
 const numbers = document.getElementsByClassName("number");
+const operators = document.getElementsByClassName("operator");
 
 const formula = document.getElementById("formula");
 const clear = document.getElementById("clear");
@@ -10,12 +11,22 @@ let ini;
 
 for (let i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener("click", () => {
-        ini = value[0] === 0 ? 1 : 10; 
+        ini = value[index] === 0 ? 1 : 10; 
         value[index] = parseInt(value[index]) * ini + parseInt(numbers[i].textContent);
-        formula.textContent = value[index];
+        formula.textContent = index === 0 ? value[index] : formula.textContent.slice(0, -(value[index].toString().length)) + value[index];
+        
     });
 }
 
+for ( let i = 0; i < operators.length; i++) {
+    operators[i].addEventListener("click", () => {
+        index++;
+        value[index] = operators[i].textContent;
+        formula.textContent = formula.textContent + value[index];
+        index++;
+        value[index] = 0;
+    })
+}
 
 clear.addEventListener("click", () => {
     value = [];
